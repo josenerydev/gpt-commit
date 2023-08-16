8f6deb7327728b323d496313abdd4449ad0ca260
Jose Nery <josenerydev@gmail.com>
2023-08-16 17:25:33 -0300

    Add license badge and section, update long description, add project details and classifiers.

--- setup.py
+++ setup.py
@@ -3,6 +3,23 @@ from setuptools import setup, find_packages
 setup(
     name='gpt-commit',
     version='0.1',
+    description="A command-line tool that intelligently generates commit messages based on staged file changes in Git using OpenAI's GPT.",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/josenerydev/gpt-commit',
+    author='José Nery',
+    author_email='josenerydev@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     packages=find_packages(),
     install_requires=[
         'openai',

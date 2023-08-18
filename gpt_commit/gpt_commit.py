0412e8d4371c630b7bc736f55ac9dccf7d9c0ca7
Jose Nery <josenerydev@gmail.com>
2023-08-17 23:31:53 -0300

    feat(gpt_commit): modify import statement for generate_commit_message
    The import statement for the function `generate_commit_message` from the module `openai_utils` has been modified to import the function `generate_single_commit_message`.

--- gpt_commit/gpt_commit.py
+++ gpt_commit/gpt_commit.py
@@ -3,19 +3,20 @@
 import asyncio
 import sys
 from .git_utils import get_diff, commit
-from .openai_utils import generate_commit_message
+from .openai_utils import generate_single_commit_message
 from .cli_parser import parse_args
 
 async def async_main():
     args = parse_args()
 
     try:
-        if not get_diff(ignore_whitespace=False):
+        diff_content = get_diff(ignore_whitespace=False)
+        if not diff_content:
             print(
                 "No changes staged. Use `git add` to stage files before invoking gpt-commit."
             )
             exit()
-        commit_message = await generate_commit_message(get_diff())
+        commit_message = await generate_single_commit_message(diff_content)
     except UnicodeDecodeError:
         print("gpt-commit does not support binary files", file=sys.stderr)
         commit_message = (
--- gpt_commit/openai_utils.py
+++ gpt_commit/openai_utils.py
@@ -1,6 +1,7 @@
 import os
 import openai
 from .diff_utils import parse_diff, assemble_diffs
+from .conventional_commits import CONVENTIONAL_COMMITS
 import asyncio
 
 DIFF_PROMPT = "Generate a succinct summary of the following code changes:"
@@ -15,13 +16,23 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 async def complete(prompt):
     model_name = "gpt-3.5-turbo"
+
     print(f"Using AI model: {model_name}")
+    print("----- Start of Prompt -----")
+    print(prompt)
+    print("----- End of Prompt -----")
+
     completion_resp = await openai.ChatCompletion.acreate(
         model=model_name,
         messages=[{"role": "user", "content": prompt[: PROMPT_CUTOFF + 100]}],
         max_tokens=128,
     )
     completion = completion_resp.choices[0].message.content.strip()
+
+    print("----- Start of OpenAI Response -----")
+    print(completion)
+    print("----- End of OpenAI Response -----")
+
     return completion
 
 
@@ -32,6 +43,20 @@ async def summarize_diff(diff):
 async def summarize_summaries(summaries):
     assert summaries
     return await complete(COMMIT_MSG_PROMPT + "\n\n" + summaries + "\n\n")
+
+async def generate_single_commit_message(diff):
+    if not diff:
+        return "Fix whitespace"
+    prompt = (
+        "Act as a software engineer who is analyzing the guide ["
+        + CONVENTIONAL_COMMITS
+        + "] in order to write a commit message that effectively describes this diff file ["
+        + diff
+        + "]. Your message should clearly and concisely capture the changes made, following all instructions and conventions outlined in the provided guide. Please be sure to avoid including any explanations or additional information about your process - only provide the generated commit message."
+    )
+    
+    # Chamamos a API com o novo prompt
+    return await complete(prompt)
 
 async def generate_commit_message(diff):
     if not diff:
